299eb0a28265ccb05da89cbea33f779c4857339c
ferenkylioe <84246538+ferenkylioe@users.noreply.github.com>
2021-05-16 18:21:01 +0700

    Initial Commit

---
+++ WebScraper/src/main/java/com/testing/scraper/demo/config/SeleniumConfiguration.java
@@ -0,0 +1,30 @@
+package com.testing.scraper.demo.config;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class SeleniumConfiguration {
+
+    private final String chromeDriverPath = "./src/main/resources/ChromeDriver90.0.4430.24.exe";
+
+    @PostConstruct
+    public void postConstruct() {
+        System.setProperty("webdriver.chrome.driver",chromeDriverPath);
+    }
+
+    @Bean
+    public ChromeDriver driver() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        //chromeOptions.addArguments("--headless");
+
+        ChromeDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        return driver;
+    }
+}
